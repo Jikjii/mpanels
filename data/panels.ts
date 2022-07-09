385288c460190df2c2b1ae2d82c7b094ece2d62e
geraldo <85137475+Jikjii@users.noreply.github.com>
2022-07-08 22:24:16 -0400

    Database Seeded

---
+++ data/panels.ts
@@ -0,0 +1,36 @@
+export const panels = [
+    {
+      category: "Anime",
+      description: "My Favorite Berserk Panel",
+      id: "8a9020b2-363b-4a4f-ad26-d6d55b51bqes",
+      imageUrl: "https://i.pinimg.com/564x/18/9b/b6/189bb6d4f1884ba9c3f78161edcb79aa.jpg",
+      title: "Femto's Birth",
+      url: "https://nextjs.org",
+    },
+    {
+      category: "Anime",
+      description: "Depression at its finest",
+      id: "2a3121b2-363b-4a4f-ad26-d6c35b41bade",
+      imageUrl: "https://i.pinimg.com/564x/5d/35/9e/5d359ec08608f61d1024c65e7dcc8cfd.jpg",
+  
+      title: "Tears, literal tears",
+      url: "https://prisma.io",
+    },
+    {
+      category: "Anime",
+      description: "Not gonna lie, I'm a huge fan of this manga",
+      id: "6a9122b2-363b-4a4f-ad26-d6c55b51baed",
+      imageUrl: "https://i.pinimg.com/564x/47/8c/f4/478cf4314c0129090ddf3906b9c6969b.jpg",
+      title: "Dandadan",
+      url: "https://tailwindcss.com",
+    },
+    {
+      category: "Anime",
+      description: "Power, Denji and Cat ?",
+      id: "2ea8cfb0-44a3-4c07-bdc2-31ffa135ea78",
+      imageUrl: "https://i.pinimg.com/564x/93/82/a3/9382a3e023e24a2c910644656b86eb72.jpg",
+      title: "Chainsaw Man",
+      url: "https://apollographql.com",
+    },
+  ];
+  
