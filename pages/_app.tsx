6bd850fd30268f582bb13954848b0eb6f8563aaf
geraldo <85137475+Jikjii@users.noreply.github.com>
2022-07-10 23:14:46 -0400

    App wrapped
    Apollo client implemented and framer motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,42 @@
+// pages/_app.tsx
 import '../styles/globals.css'
+import { UserProvider } from '@auth0/nextjs-auth0'
 import Layout from '../components/Layout'
 import { ApolloProvider } from '@apollo/client'
 import apolloClient from '../lib/apollo'
+import { AnimatePresence } from 'framer-motion'
+import { useRouter } from "next/router";
+import React from 'react'
 
 function MyApp({ Component, pageProps }) {
+
+  const [isFirstMount, setIsFirstMount] = React.useState(true);
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleRouteChange = () => {
+      isFirstMount && setIsFirstMount(false);
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method:
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, []);
   return (
-    <ApolloProvider client={apolloClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloProvider>
+    <UserProvider>
+      <ApolloProvider client={apolloClient}>
+        <Layout>
+          <AnimatePresence exitBeforeEnter>
+          <Component isFirstMount={isFirstMount}
+          key={router.route} {...pageProps} />
+          </AnimatePresence>
+        </Layout>
+      </ApolloProvider>
+    </UserProvider>
   )
 }
 
